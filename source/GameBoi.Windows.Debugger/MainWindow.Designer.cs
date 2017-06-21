0cc38fb5f7e299c8e94268f141d028e1b202f640
Evan Bass <evanbbass@gmail.com>
2017-06-20 20:02:57 -0400

    added disassembly to form

--- source/GameBoi.Windows.Debugger/MainWindow.Designer.cs
+++ source/GameBoi.Windows.Debugger/MainWindow.Designer.cs
@@ -50,6 +50,12 @@
 			this.textBoxRegA = new System.Windows.Forms.TextBox();
 			this.labelRegAF = new System.Windows.Forms.Label();
 			this.buttonStepCPU = new System.Windows.Forms.Button();
+			this.buttonLoadCartridge = new System.Windows.Forms.Button();
+			this.labelLastInstruction = new System.Windows.Forms.Label();
+			this.textBoxLastInstruction = new System.Windows.Forms.TextBox();
+			this.textBoxNextInstruction = new System.Windows.Forms.TextBox();
+			this.labelNextInstruction = new System.Windows.Forms.Label();
+			this.buttonReset = new System.Windows.Forms.Button();
 			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -88,7 +94,7 @@
 			this.checkBoxCarryFlag.Location = new System.Drawing.Point(136, 108);
 			this.checkBoxCarryFlag.Name = "checkBoxCarryFlag";
 			this.checkBoxCarryFlag.Size = new System.Drawing.Size(95, 21);
-			this.checkBoxCarryFlag.TabIndex = 20;
+			this.checkBoxCarryFlag.TabIndex = 14;
 			this.checkBoxCarryFlag.Text = "Carry Flag";
 			this.checkBoxCarryFlag.UseVisualStyleBackColor = true;
 			this.checkBoxCarryFlag.CheckedChanged += new System.EventHandler(this.checkBoxCarryFlag_CheckedChanged);
@@ -99,7 +105,7 @@
 			this.checkBoxHalfCarryFlag.Location = new System.Drawing.Point(136, 80);
 			this.checkBoxHalfCarryFlag.Name = "checkBoxHalfCarryFlag";
 			this.checkBoxHalfCarryFlag.Size = new System.Drawing.Size(125, 21);
-			this.checkBoxHalfCarryFlag.TabIndex = 19;
+			this.checkBoxHalfCarryFlag.TabIndex = 13;
 			this.checkBoxHalfCarryFlag.Text = "Half-Carry Flag";
 			this.checkBoxHalfCarryFlag.UseVisualStyleBackColor = true;
 			this.checkBoxHalfCarryFlag.CheckedChanged += new System.EventHandler(this.checkBoxHalfCarryFlag_CheckedChanged);
@@ -110,7 +116,7 @@
 			this.checkBoxSubtractFlag.Location = new System.Drawing.Point(136, 52);
 			this.checkBoxSubtractFlag.Name = "checkBoxSubtractFlag";
 			this.checkBoxSubtractFlag.Size = new System.Drawing.Size(114, 21);
-			this.checkBoxSubtractFlag.TabIndex = 18;
+			this.checkBoxSubtractFlag.TabIndex = 12;
 			this.checkBoxSubtractFlag.Text = "Subtract Flag";
 			this.checkBoxSubtractFlag.UseVisualStyleBackColor = true;
 			this.checkBoxSubtractFlag.CheckedChanged += new System.EventHandler(this.checkBoxSubtractFlag_CheckedChanged);
@@ -121,7 +127,7 @@
 			this.checkBoxZeroFlag.Location = new System.Drawing.Point(136, 24);
 			this.checkBoxZeroFlag.Name = "checkBoxZeroFlag";
 			this.checkBoxZeroFlag.Size = new System.Drawing.Size(91, 21);
-			this.checkBoxZeroFlag.TabIndex = 17;
+			this.checkBoxZeroFlag.TabIndex = 11;
 			this.checkBoxZeroFlag.Text = "Zero Flag";
 			this.checkBoxZeroFlag.UseVisualStyleBackColor = true;
 			this.checkBoxZeroFlag.CheckedChanged += new System.EventHandler(this.checkBoxZeroFlag_CheckedChanged);
@@ -129,11 +135,12 @@
 			// textBoxRegSP
 			// 
 			this.textBoxRegSP.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegSP.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegSP.Location = new System.Drawing.Point(39, 134);
 			this.textBoxRegSP.MaxLength = 4;
 			this.textBoxRegSP.Name = "textBoxRegSP";
-			this.textBoxRegSP.Size = new System.Drawing.Size(61, 22);
-			this.textBoxRegSP.TabIndex = 15;
+			this.textBoxRegSP.Size = new System.Drawing.Size(61, 24);
+			this.textBoxRegSP.TabIndex = 9;
 			this.textBoxRegSP.Text = "0000";
 			this.textBoxRegSP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			this.textBoxRegSP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxReg_KeyPress);
@@ -151,11 +158,12 @@
 			// textBoxRegPC
 			// 
 			this.textBoxRegPC.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegPC.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegPC.Location = new System.Drawing.Point(39, 162);
 			this.textBoxRegPC.MaxLength = 4;
 			this.textBoxRegPC.Name = "textBoxRegPC";
-			this.textBoxRegPC.Size = new System.Drawing.Size(61, 22);
-			this.textBoxRegPC.TabIndex = 13;
+			this.textBoxRegPC.Size = new System.Drawing.Size(61, 24);
+			this.textBoxRegPC.TabIndex = 10;
 			this.textBoxRegPC.Text = "0000";
 			this.textBoxRegPC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			this.textBoxRegPC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxReg_KeyPress);
@@ -173,11 +181,12 @@
 			// textBoxRegL
 			// 
 			this.textBoxRegL.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegL.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegL.Location = new System.Drawing.Point(70, 106);
 			this.textBoxRegL.MaxLength = 2;
 			this.textBoxRegL.Name = "textBoxRegL";
-			this.textBoxRegL.Size = new System.Drawing.Size(30, 22);
-			this.textBoxRegL.TabIndex = 11;
+			this.textBoxRegL.Size = new System.Drawing.Size(30, 24);
+			this.textBoxRegL.TabIndex = 8;
 			this.textBoxRegL.Text = "00";
 			this.textBoxRegL.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			this.textBoxRegL.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxReg_KeyPress);
@@ -186,11 +195,12 @@
 			// textBoxRegH
 			// 
 			this.textBoxRegH.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegH.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegH.Location = new System.Drawing.Point(39, 106);
 			this.textBoxRegH.MaxLength = 2;
 			this.textBoxRegH.Name = "textBoxRegH";
-			this.textBoxRegH.Size = new System.Drawing.Size(30, 22);
-			this.textBoxRegH.TabIndex = 10;
+			this.textBoxRegH.Size = new System.Drawing.Size(30, 24);
+			this.textBoxRegH.TabIndex = 7;
 			this.textBoxRegH.Text = "00";
 			this.textBoxRegH.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			this.textBoxRegH.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxReg_KeyPress);
@@ -208,11 +218,12 @@
 			// textBoxRegE
 			// 
 			this.textBoxRegE.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegE.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegE.Location = new System.Drawing.Point(70, 78);
 			this.textBoxRegE.MaxLength = 2;
 			this.textBoxRegE.Name = "textBoxRegE";
-			this.textBoxRegE.Size = new System.Drawing.Size(30, 22);
-			this.textBoxRegE.TabIndex = 8;
+			this.textBoxRegE.Size = new System.Drawing.Size(30, 24);
+			this.textBoxRegE.TabIndex = 6;
 			this.textBoxRegE.Text = "00";
 			this.textBoxRegE.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			this.textBoxRegE.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxReg_KeyPress);
@@ -221,11 +232,12 @@
 			// textBoxRegD
 			// 
 			this.textBoxRegD.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegD.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegD.Location = new System.Drawing.Point(39, 78);
 			this.textBoxRegD.MaxLength = 2;
 			this.textBoxRegD.Name = "textBoxRegD";
-			this.textBoxRegD.Size = new System.Drawing.Size(30, 22);
-			this.textBoxRegD.TabIndex = 7;
+			this.textBoxRegD.Size = new System.Drawing.Size(30, 24);
+			this.textBoxRegD.TabIndex = 5;
 			this.textBoxRegD.Text = "00";
 			this.textBoxRegD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			this.textBoxRegD.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxReg_KeyPress);
@@ -243,11 +255,12 @@
 			// textBoxRegC
 			// 
 			this.textBoxRegC.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegC.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegC.Location = new System.Drawing.Point(70, 50);
 			this.textBoxRegC.MaxLength = 2;
 			this.textBoxRegC.Name = "textBoxRegC";
-			this.textBoxRegC.Size = new System.Drawing.Size(30, 22);
-			this.textBoxRegC.TabIndex = 5;
+			this.textBoxRegC.Size = new System.Drawing.Size(30, 24);
+			this.textBoxRegC.TabIndex = 4;
 			this.textBoxRegC.Text = "00";
 			this.textBoxRegC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			this.textBoxRegC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxReg_KeyPress);
@@ -256,11 +269,12 @@
 			// textBoxRegB
 			// 
 			this.textBoxRegB.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegB.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegB.Location = new System.Drawing.Point(39, 50);
 			this.textBoxRegB.MaxLength = 2;
 			this.textBoxRegB.Name = "textBoxRegB";
-			this.textBoxRegB.Size = new System.Drawing.Size(30, 22);
-			this.textBoxRegB.TabIndex = 4;
+			this.textBoxRegB.Size = new System.Drawing.Size(30, 24);
+			this.textBoxRegB.TabIndex = 3;
 			this.textBoxRegB.Text = "00";
 			this.textBoxRegB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			this.textBoxRegB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxReg_KeyPress);
@@ -278,10 +292,11 @@
 			// textBoxRegF
 			// 
 			this.textBoxRegF.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegF.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegF.Location = new System.Drawing.Point(70, 22);
 			this.textBoxRegF.MaxLength = 2;
 			this.textBoxRegF.Name = "textBoxRegF";
-			this.textBoxRegF.Size = new System.Drawing.Size(30, 22);
+			this.textBoxRegF.Size = new System.Drawing.Size(30, 24);
 			this.textBoxRegF.TabIndex = 2;
 			this.textBoxRegF.Text = "00";
 			this.textBoxRegF.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -291,10 +306,11 @@
 			// textBoxRegA
 			// 
 			this.textBoxRegA.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.textBoxRegA.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxRegA.Location = new System.Drawing.Point(39, 22);
 			this.textBoxRegA.MaxLength = 2;
 			this.textBoxRegA.Name = "textBoxRegA";
-			this.textBoxRegA.Size = new System.Drawing.Size(30, 22);
+			this.textBoxRegA.Size = new System.Drawing.Size(30, 24);
 			this.textBoxRegA.TabIndex = 1;
 			this.textBoxRegA.Text = "00";
 			this.textBoxRegA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -312,19 +328,81 @@
 			// 
 			// buttonStepCPU
 			// 
-			this.buttonStepCPU.Location = new System.Drawing.Point(894, 684);
+			this.buttonStepCPU.Location = new System.Drawing.Point(538, 176);
 			this.buttonStepCPU.Name = "buttonStepCPU";
-			this.buttonStepCPU.Size = new System.Drawing.Size(100, 25);
-			this.buttonStepCPU.TabIndex = 2;
+			this.buttonStepCPU.Size = new System.Drawing.Size(100, 30);
+			this.buttonStepCPU.TabIndex = 15;
 			this.buttonStepCPU.Text = "Step CPU";
 			this.buttonStepCPU.UseVisualStyleBackColor = true;
 			this.buttonStepCPU.Click += new System.EventHandler(this.buttonStepCPU_Click);
+			// 
+			// buttonLoadCartridge
+			// 
+			this.buttonLoadCartridge.Location = new System.Drawing.Point(407, 176);
+			this.buttonLoadCartridge.Name = "buttonLoadCartridge";
+			this.buttonLoadCartridge.Size = new System.Drawing.Size(125, 30);
+			this.buttonLoadCartridge.TabIndex = 16;
+			this.buttonLoadCartridge.Text = "Load Cartridge...";
+			this.buttonLoadCartridge.UseVisualStyleBackColor = true;
+			this.buttonLoadCartridge.Click += new System.EventHandler(this.buttonLoadCartridge_Click);
+			// 
+			// labelLastInstruction
+			// 
+			this.labelLastInstruction.AutoSize = true;
+			this.labelLastInstruction.Location = new System.Drawing.Point(285, 37);
+			this.labelLastInstruction.Name = "labelLastInstruction";
+			this.labelLastInstruction.Size = new System.Drawing.Size(104, 17);
+			this.labelLastInstruction.TabIndex = 17;
+			this.labelLastInstruction.Text = "Last Instruction";
+			// 
+			// textBoxLastInstruction
+			// 
+			this.textBoxLastInstruction.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxLastInstruction.Location = new System.Drawing.Point(288, 62);
+			this.textBoxLastInstruction.Name = "textBoxLastInstruction";
+			this.textBoxLastInstruction.ReadOnly = true;
+			this.textBoxLastInstruction.Size = new System.Drawing.Size(350, 24);
+			this.textBoxLastInstruction.TabIndex = 18;
+			// 
+			// textBoxNextInstruction
+			// 
+			this.textBoxNextInstruction.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxNextInstruction.Location = new System.Drawing.Point(288, 118);
+			this.textBoxNextInstruction.Name = "textBoxNextInstruction";
+			this.textBoxNextInstruction.ReadOnly = true;
+			this.textBoxNextInstruction.Size = new System.Drawing.Size(350, 24);
+			this.textBoxNextInstruction.TabIndex = 20;
+			// 
+			// labelNextInstruction
+			// 
+			this.labelNextInstruction.AutoSize = true;
+			this.labelNextInstruction.Location = new System.Drawing.Point(285, 93);
+			this.labelNextInstruction.Name = "labelNextInstruction";
+			this.labelNextInstruction.Size = new System.Drawing.Size(105, 17);
+			this.labelNextInstruction.TabIndex = 19;
+			this.labelNextInstruction.Text = "Next Instruction";
+			// 
+			// buttonReset
+			// 
+			this.buttonReset.Location = new System.Drawing.Point(326, 176);
+			this.buttonReset.Name = "buttonReset";
+			this.buttonReset.Size = new System.Drawing.Size(75, 30);
+			this.buttonReset.TabIndex = 21;
+			this.buttonReset.Text = "Reset";
+			this.buttonReset.UseVisualStyleBackColor = true;
+			this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
 			// 
 			// MainWindow
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1006, 721);
+			this.ClientSize = new System.Drawing.Size(650, 218);
+			this.Controls.Add(this.buttonReset);
+			this.Controls.Add(this.textBoxNextInstruction);
+			this.Controls.Add(this.labelNextInstruction);
+			this.Controls.Add(this.textBoxLastInstruction);
+			this.Controls.Add(this.labelLastInstruction);
+			this.Controls.Add(this.buttonLoadCartridge);
 			this.Controls.Add(this.buttonStepCPU);
 			this.Controls.Add(this.groupBox1);
 			this.Name = "MainWindow";
@@ -332,6 +410,7 @@
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -359,6 +438,12 @@
 		private System.Windows.Forms.TextBox textBoxRegA;
 		private System.Windows.Forms.Label labelRegAF;
 		private System.Windows.Forms.Button buttonStepCPU;
+		private System.Windows.Forms.Button buttonLoadCartridge;
+		private System.Windows.Forms.Label labelLastInstruction;
+		private System.Windows.Forms.TextBox textBoxLastInstruction;
+		private System.Windows.Forms.TextBox textBoxNextInstruction;
+		private System.Windows.Forms.Label labelNextInstruction;
+		private System.Windows.Forms.Button buttonReset;
 	}
 }
 
--- source/GameBoi.Windows.Debugger/MainWindow.cs
+++ source/GameBoi.Windows.Debugger/MainWindow.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
-
+using GameBoi.Windows.Debugger.Properties;
 using GameBoiManaged;
 
 namespace GameBoi.Windows.Debugger
@@ -10,12 +11,14 @@ namespace GameBoi.Windows.Debugger
 	public partial class MainWindow : Form
 	{
 		private readonly GameBoyManaged mGameBoy;
+		private string mLastInstruction;
+		private string mNextInstruction;
 
 		public MainWindow()
 		{
 			mGameBoy = new GameBoyManaged();
 			InitializeComponent();
-			UpdateFields();
+			Reset();
 		}
 
 		private void textBoxReg_KeyPress(object sender, KeyPressEventArgs e)
@@ -31,6 +34,37 @@ namespace GameBoi.Windows.Debugger
 			{
 				e.Handled = true;
 			}
+		}
+
+		private void Reset()
+		{
+			mGameBoy.Reset();
+			mLastInstruction = String.Empty;
+			mNextInstruction = String.Empty;
+			UpdateDisassembly();
+			UpdateFields();
+		}
+
+		private void UpdateDisassembly()
+		{
+			mLastInstruction = mNextInstruction;
+
+			byte opcode = mGameBoy.MemoryMap.ReadByte(mGameBoy.CPU.Registers.PC);
+			int operandLength = CPUManaged.GetOperandLength(opcode);
+			ushort operand;
+			switch (operandLength)
+			{
+				case 1:
+					operand = mGameBoy.MemoryMap.ReadByte((ushort)(mGameBoy.CPU.Registers.PC + 1));
+					break;
+				case 2:
+					operand = mGameBoy.MemoryMap.ReadWord((ushort)(mGameBoy.CPU.Registers.PC + 1));
+					break;
+				default:
+					operand = 0x0000;
+					break;
+			}
+			mNextInstruction = $"0x{mGameBoy.CPU.Registers.PC:X4}: {CPUManaged.GetDisassembly(opcode, operand)}";
 		}
 
 		private void UpdateFields()
@@ -50,73 +84,104 @@ namespace GameBoi.Windows.Debugger
 			checkBoxSubtractFlag.Checked = registers.SubtractFlag;
 			checkBoxHalfCarryFlag.Checked = registers.HalfCarryFlag;
 			checkBoxCarryFlag.Checked = registers.CarryFlag;
+
+			textBoxLastInstruction.Text = mLastInstruction;
+			textBoxNextInstruction.Text = mNextInstruction;
+		}
+
+		private void buttonReset_Click(object sender, EventArgs e)
+		{
+			Reset();
+		}
+
+		private void buttonLoadCartridge_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog
+			{
+				Title = Resources.MainWindow_buttonLoadCartridge_Title,
+				Filter = Resources.MainWindow_buttonLoadCartridge_Filter,
+				Multiselect = false
+			};
+
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				mGameBoy.LoadCartridge(ofd.FileName);
+				Reset();
+				Text = $"GameBoi - Debugging \"{mGameBoy.MemoryMap.Cartridge.GameTitle}\"";
+			}
 		}
 
 		private void buttonStepCPU_Click(object sender, EventArgs e)
 		{
 			mGameBoy.CPU.StepCPU();
+			UpdateDisassembly();
 			UpdateFields();
 		}
 
-		private void textBoxRegA_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegA_Validating(object sender, CancelEventArgs e)
 		{
 			mGameBoy.CPU.Registers.A = Byte.Parse(textBoxRegA.Text, NumberStyles.HexNumber);
 			UpdateFields();
 		}
 
-		private void textBoxRegF_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegF_Validating(object sender, CancelEventArgs e)
 		{
 			mGameBoy.CPU.Registers.F = Byte.Parse(textBoxRegF.Text, NumberStyles.HexNumber);
 			mGameBoy.CPU.Registers.F &= 0xF0;
 			UpdateFields();
 		}
 
-		private void textBoxRegB_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegB_Validating(object sender, CancelEventArgs e)
 		{
 			mGameBoy.CPU.Registers.B = Byte.Parse(textBoxRegB.Text, NumberStyles.HexNumber);
 			UpdateFields();
 		}
 
-		private void textBoxRegC_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegC_Validating(object sender, CancelEventArgs e)
 		{
 			mGameBoy.CPU.Registers.C = Byte.Parse(textBoxRegC.Text, NumberStyles.HexNumber);
 			UpdateFields();
 		}
 
-		private void textBoxRegD_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegD_Validating(object sender, CancelEventArgs e)
 		{
 			mGameBoy.CPU.Registers.D = Byte.Parse(textBoxRegD.Text, NumberStyles.HexNumber);
 			UpdateFields();
 		}
 
-		private void textBoxRegE_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegE_Validating(object sender, CancelEventArgs e)
 		{
 			mGameBoy.CPU.Registers.E = Byte.Parse(textBoxRegE.Text, NumberStyles.HexNumber);
 			UpdateFields();
 		}
 
-		private void textBoxRegH_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegH_Validating(object sender, CancelEventArgs e)
 		{
 			mGameBoy.CPU.Registers.H = Byte.Parse(textBoxRegH.Text, NumberStyles.HexNumber);
 			UpdateFields();
 		}
 
-		private void textBoxRegL_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegL_Validating(object sender, CancelEventArgs e)
 		{
 			mGameBoy.CPU.Registers.L = Byte.Parse(textBoxRegL.Text, NumberStyles.HexNumber);
 			UpdateFields();
 		}
 
-		private void textBoxRegSP_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegSP_Validating(object sender, CancelEventArgs e)
 		{
 			mGameBoy.CPU.Registers.SP = UInt16.Parse(textBoxRegSP.Text, NumberStyles.HexNumber);
 			UpdateFields();
 		}
 
-		private void textBoxRegPC_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+		private void textBoxRegPC_Validating(object sender, CancelEventArgs e)
 		{
+			// TODO This needs to be validated
+
 			mGameBoy.CPU.Registers.PC = UInt16.Parse(textBoxRegPC.Text, NumberStyles.HexNumber);
 			UpdateFields();
+
+			// invalidate previous instruction since we don't know what it was
+			mLastInstruction = String.Empty;
 		}
 
 		private void checkBoxZeroFlag_CheckedChanged(object sender, EventArgs e)
